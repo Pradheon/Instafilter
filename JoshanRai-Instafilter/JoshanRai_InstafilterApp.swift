b2eaddde4c769019d0849d4e9ebdacc04de917ed
Joshan Rai <pradheon@gmail.com>
2022-04-11 15:01:51 -0700

    Initial Commit

---
+++ JoshanRai-Instafilter/JoshanRai_InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoshanRai_InstafilterApp.swift
+//  JoshanRai-Instafilter
+//
+//  Created by Joshan Rai on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct JoshanRai_InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
