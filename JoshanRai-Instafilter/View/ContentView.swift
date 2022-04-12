88ebb90828356d8c24a165554daa36007fdb45b9
Joshan Rai <pradheon@gmail.com>
2022-04-11 17:12:35 -0700

    v0.1
    - How property wrappers become structs
    - https://www.hackingwithswift.com/books/ios-swiftui/how-property-wrappers-become-structs

---
+++ JoshanRai-Instafilter/View/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  JoshanRai-Instafilter
+//
+//  Created by Joshan Rai on 4/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var blurAmount = 0.0 {
+        didSet {
+            print("New value is \(blurAmount)")
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .blur(radius: blurAmount)
+            
+            Slider(value: $blurAmount, in: 0...20)
+                .padding()
+            
+            Button("Random Blur") {
+                blurAmount = Double.random(in: 0...20)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
